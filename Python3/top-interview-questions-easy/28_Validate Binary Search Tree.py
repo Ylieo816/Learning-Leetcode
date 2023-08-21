ef2d40465d03668a841d37a458f0801eafd9175a
Ylieo <leoyao816@gmail.com>
2023-08-21 22:25:57 +0800

    Practice Leetcode on 20230821

--- Python3/top-interview-questions-easy/28_Validate Binary Search Tree.py
+++ Python3/top-interview-questions-easy/28_Validate Binary Search Tree.py
@@ -1,2 +1,20 @@
 # Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
 
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        if not root:
+            return True
+        return self.mirror(root.left, root.right)
+        
+    def mirror(self,left,right):
+        if not left or not right:
+            return left == right
+        if left.val != right.val:
+            return False
+        return self.mirror(left.left, right.right) and self.mirror(left.right, right.left)
