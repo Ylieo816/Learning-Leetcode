108cb00a9c25d778bfe0f11f3ca3fcfdcffcb40f
Ylieo <leoyao816@gmail.com>
2023-08-24 18:17:28 +0800

    Practice Leetcode on 20230824

--- Python3/top-interview-questions-easy/37_Shuffle an Array.py
+++ Python3/top-interview-questions-easy/37_Shuffle an Array.py
@@ -6,3 +6,27 @@
 #     - int[] reset() Resets the array to its original configuration and returns it.
 #     - int[] shuffle() Returns a random shuffling of the array.
 
+class Solution:
+
+    def __init__(self, nums: List[int]):
+        self.nums = nums
+
+    def reset(self) -> List[int]:
+        """
+        Resets the array to its original configuration and return it.
+        """
+        return self.nums
+
+    def shuffle(self) -> List[int]:
+        """
+        Returns a random shuffling of the array.
+        """
+        nums_new = self.nums[:]
+        random.shuffle(nums_new)
+        return nums_new
+
+
+# Your Solution object will be instantiated and called as such:
+# obj = Solution(nums)
+# param_1 = obj.reset()
+# param_2 = obj.shuffle()
