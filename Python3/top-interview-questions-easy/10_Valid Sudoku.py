4a4fdf06f5deeeb34165ead2edc8084d1c306731
Ylieo <leoyao816@gmail.com>
2023-11-13 22:53:38 +0800

    [20231113] Leetcode - review 1

--- Python3/top-interview-questions-easy/10_Valid Sudoku.py
+++ Python3/top-interview-questions-easy/10_Valid Sudoku.py
@@ -8,37 +8,58 @@
 # A Sudoku board (partially filled) could be valid but is not necessarily solvable.
 # Only the filled cells need to be validated according to the mentioned rules.
 
-# class Solution:
-#     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        # for i in range(9):
-        #     value_row = []
-        #     value_col = []
-        #     value_box = []
-        #     for j in range(9):
-        #         if board[i][j] != ".":
-        #             if board[i][j] not in value_row:
-        #                 value_row.append(board[i][j])
-        #             else:
-        #                 return False
-        #         if board[j][i] != ".":
-        #             if board[j][i] not in value_col:
-        #                 value_col.append(board[j][i])
-        #             else:
-        #                 return False
-        #         if board[((i//3)*3)+(j//3)][(j%3)+(3*i)%9] != ".":
-        #             if board[((i//3)*3)+(j//3)][(j%3)+(3*i)%9] not in value_box:
-        #                 value_box.append(board[((i//3)*3)+(j//3)][(j%3)+(3*i)%9])
-        #             else:
-        #                 return False
-        # return True
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        # "res" will be used to store all the valid elements in the board
+        res = []
+        # loops through each cell in the board
+        for i in range(9):
+            for j in range(9):
+                # not a dot ('.'), which means it's a valid number
+                if board[i][j] != '.':
+                    res += [(i, board[i][j]), (board[i][j], j), (i // 3, j // 3, board[i][j])]
+        return len(res) == len(set(res))
+
+# Time complexity: O(n∗n)
+# Space complexity: O(n*n)
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        rows = [set() for _ in range(9)]
+        cols = [set() for _ in range(9)]
+        block = [[set() for _ in range(3)] for _ in range(3)]
+
+        for i in range(9):
+            for j in range(9):
+                curr = board[i][j]
+                if curr == '.':
+                    continue
+                if (curr in rows[i]) or (curr in cols[j]) or (curr in block[i // 3][j // 3]):
+                    return False
+                rows[i].add(curr)
+                cols[j].add(curr)
+                block[i // 3][j // 3].add(curr)
+        return True
 
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
-        res = []
         for i in range(9):
+            value_row = []
+            value_col = []
+            value_box = []
             for j in range(9):
-                element = board[i][j]
-                if element != '.':
-                    res += [(i, element), (element, j), (i // 3, j // 3, element)]
-        return len(res) == len(set(res))
-
+                if board[i][j] != ".":
+                    if board[i][j] not in value_row:
+                        value_row.append(board[i][j])
+                    else:
+                        return False
+                if board[j][i] != ".":
+                    if board[j][i] not in value_col:
+                        value_col.append(board[j][i])
+                    else:
+                        return False
+                if board[((i//3)*3)+(j//3)][(j%3)+(3*i)%9] != ".":
+                    if board[((i//3)*3)+(j//3)][(j%3)+(3*i)%9] not in value_box:
+                        value_box.append(board[((i//3)*3)+(j//3)][(j%3)+(3*i)%9])
+                    else:
+                        return False
+        return True
--- README.md
+++ README.md
@@ -6,14 +6,20 @@ Topic
 - Top Interview Questions - [Easy Collection](https://leetcode.com/explore/featured/card/top-interview-questions-easy/) (48) - [2023/08/09 - 2023/08/28]
 - Blind 75 LeetCode Questions (75) - [2023/08/29 - 2023/10/15]
 - Top Interview Questions - [Medium Collection](https://leetcode.com/explore/interview/card/top-interview-questions-medium/) (52) - [2023/10/16 - 2023/11/10]
-- [Dynamic Programming](https://leetcode.com/explore/featured/card/dynamic-programming/) (55)
-- [Recursion I](https://leetcode.com/explore/featured/card/recursion-i/) (21)
+
+Review - 1 round [2023/11/11 - 2023/11/17]
+
+- (Concept) [Dynamic Programming](https://leetcode.com/explore/featured/card/dynamic-programming/) (55) [2023/11/18 - 2023/11/19]
+- (Concept) [Heap](https://leetcode.com/explore/featured/card/heap/) (28) [2023/11/20 - 2023/11/21]
+- [Coding Interview Strategy](https://leetcode.com/explore/interview/card/coding-interview-strategy/) (9) [2023/11/22 - 2023/11/24]
+- [Recursion I](https://leetcode.com/explore/featured/card/recursion-i/) (21) [2023/11/25 - ]
 - [Sorting](https://leetcode.com/explore/learn/card/sorting/) (20)
-- [System Design](https://leetcode.com/explore/learn/card/system-design/) (42)
-- [Coding Interview Strategy](https://leetcode.com/explore/interview/card/coding-interview-strategy/) (9)
+- (Concept) [System Design](https://leetcode.com/explore/learn/card/system-design/) (42)
 - [Binary Tree](https://leetcode.com/explore/learn/card/data-structure-tree/) (19)
-- [Heap](https://leetcode.com/explore/featured/card/heap/) (28)
-- [Graph](https://leetcode.com/explore/featured/card/graph/) (58)
+- [Linked List](https://leetcode.com/explore/learn/card/linked-list/) (30)
+- (Concept) [Graph](https://leetcode.com/explore/featured/card/graph/) (58)
+- (Concept) [Hash Table](https://leetcode.com/explore/learn/card/hash-table/) (35) Part
+- (Concept) [Array and String](https://leetcode.com/explore/learn/card/array-and-string/) (35) Part
 - [Queue & Stack](https://leetcode.com/explore/learn/card/queue-stack/) (32)
 - [Binary Search](https://leetcode.com/explore/learn/card/binary-search/) (30)
 - [Recursion II](https://leetcode.com/explore/learn/card/recursion-ii/) (25)
