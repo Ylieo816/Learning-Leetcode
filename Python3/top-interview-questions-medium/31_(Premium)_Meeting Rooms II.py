7ba471a1fc5bd0d54439da04d226f7a2cc40f054
Ylieo <leoyao816@gmail.com>
2023-12-21 20:34:24 +0800

    [20231221] Leetcode - add premium - (31,37)

--- Python3/top-interview-questions-medium/31_(Premium)_Meeting Rooms II.py
+++ Python3/top-interview-questions-medium/31_(Premium)_Meeting Rooms II.py
@@ -1,1 +1,33 @@
 # Given an array of meeting time intervals intervals where intervals[i] = [starti, endi], return the minimum number of conference rooms required.
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[List[int]]) -> int:
+        # Heap
+        """
+        [[0,30],[5,10],[9,15],[15,18],[16,18],[19,20],[40,41] ]
+        [0                                    30]
+            [5  10]
+               [9    15]
+                     [15     18]
+                         [16 18]
+                                  [19 20] 
+                                                   [40  41]
+        [0,30],  h =[30]
+        [5,10],  h =[10,30]
+        [9,15],  h =[10,15,30]
+        [15,18], h =[15,18,30]
+        [16,18], h =[18,18,30]
+        [19,20], h= [18,20,30]
+		[40,41], h =[20,30,40]
+        """
+        heap = []
+        intervals.sort()
+        for interval in intervals:
+            if not heap or heap[0] > interval[0]:
+                # need a new meeting room
+                heapq.heappush(heap, interval[1])
+            else:
+                # don't need a new meeting room, just update the end time
+                heapq.heapreplace(heap, interval[1])
+
+        return len(heap)
--- Python3/top-interview-questions-medium/37_(Premium)_Flatten 2D Vector.py
+++ Python3/top-interview-questions-medium/37_(Premium)_Flatten 2D Vector.py
@@ -1,2 +1,53 @@
-# Implement an iterator to flatten a 2d vector.
+# Design an iterator to flatten a 2D vector. It should support the next and hasNext operations.
 
+# Implement the Vector2D class:
+
+# Vector2D(int[][] vec) initializes the object with the 2D vector vec.
+# next() returns the next element from the 2D vector and moves the pointer one step forward. You may assume that all the calls to next are valid.
+# hasNext() returns true if there are still some elements in the vector, and false otherwise.
+
+class Vector2D:
+# Time Complexity: O(1)
+# Space Complexity: O(v.size)
+    def __init__(self, vec: List[List[int]]):
+        self.ptr = -1
+        self.table = []
+        for i in range(len(vec)):
+            self.table = self.table + vec[i]
+
+    def next(self) -> int:
+        self.ptr += 1
+        return self.table[self.ptr]
+
+    def hasNext(self) -> bool:
+        if (self.ptr+1)  >= len(self.table):
+            return False
+        else:
+            return True
+
+class Vector2D:
+# Time Complexity: O(1)
+# Space Complexity: O(1)
+    def __init__(self, v: List[List[int]]):
+        self.table = v
+        self.col = 0
+        self.row = 0
+
+    def next(self) -> int:
+        self.hasNext()
+        val = self.table[self.row][self.col]
+        self.col += 1
+        return val
+
+    def hasNext(self) -> bool:
+        while self.row < len(self.table):
+            if self.col < len(self.table[self.row]):
+                return True
+            self.row += 1
+            self.col = 0
+        return False
+
+# Your Vector2D object will be instantiated and called as such:
+# obj = Vector2D(vec)
+# param_1 = obj.next()
+# param_2 = obj.hasNext()
